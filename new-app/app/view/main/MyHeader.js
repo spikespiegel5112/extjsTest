522abff59f0ea22542f9bf8c91f9897bea44fb40
spikespiegel5112 <583288277@qq.com>
2017-09-07 18:15:26 +0800

    temp

---
+++ new-app/app/view/main/MyHeader.js
@@ -0,0 +1,27 @@
+var button = Ext.create('Ext.Button', {
+    text: 'Button'
+});
+
+Ext.define('app.view.main.MyHeader',{
+    extend: 'Ext.panel.Panel',
+    xtype:'MyHeader',
+    title: '上海千人计划平台评审系统',
+    layout: {
+        type: 'hbox'
+    },
+    width: 400,
+    renderTo: Ext.getBody(),
+    border: 0,
+    margin: '0 0 10px 0',
+    style: {boxShadow:'0 5px 5px #ccc'},
+    defaults: {
+        labelWidth: 80,
+        // implicitly create Container by specifying xtype
+        xtype: 'datefield',
+        flex: 1,
+        style: {
+            padding: '10px'
+        }
+    },
+
+})
